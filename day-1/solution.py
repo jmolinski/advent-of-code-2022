45cd4d9f9e19ad3f98e7ff334063011c93f56f80
Jakub Molinski <jakub@molinski.dev>
2022-12-02 22:48:02 +0100

    Day 1

---
+++ day-1/solution.py
@@ -0,0 +1,5 @@
+with open("input.txt") as f:
+    elfs = [[int(cal) for cal in e.strip().split('\n')] for e in f.read().strip().split('\n\n')]
+
+print("Part 1", max(sum(elf) for elf in elfs))
+print("Part 2", sum(sorted(sum(elf) for elf in elfs)[-3:]))
